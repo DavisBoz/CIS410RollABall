7d2b4cafe6317cf4763abeca8fad1ee67b3f757f
DavisBoz <58324603+DavisBoz@users.noreply.github.com>
2020-04-03 16:50:03 -0700

    Add files via upload

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    private int Jumped;
+    private Rigidbody rb;
+    private int count;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+   
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+        if (rb.transform.position.y == 0.5f)
+            Jumped = 0;
+        if (Input.GetKeyDown("space") && Jumped < 2)
+        {
+            Vector3 jump = new Vector3(0.0f, 200.0f, 0.0f);
+            rb.AddForce(jump);
+            Jumped += 1;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
